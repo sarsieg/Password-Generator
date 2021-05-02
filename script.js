887cde8b74745a86611e18a6d4ed9beb2e6fdef5
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-05-01 21:41:40 -0500

    added in values for special, numeric and alphabetical characters

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ let confirmCharacter;
 let confirmUppercase;
 let confirmLowercase;
 
+// Variable values:
+// Added values for special characters, numeric characters, and alphabetical characters
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+space = [];
+let choices;
+
+// Added to convert letters to uppercase
+let toUpper = function (x) {
+    return x.toUpperCase();
+};
+
+
